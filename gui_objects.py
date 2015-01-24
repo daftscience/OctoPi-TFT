a4c8dcba8773d5f159d50a02703277cdea8eb421
Tom <tommy@daftscience.com>
2015-01-23 23:10:17 -0600

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	constants.py
    	default.py
    	displayscreen.py
    	eztext.py
    	gui_objects.py
    	pitftgpio.py
    	plugins/file/config/screen.ini
    	plugins/file/screen.py
    	plugins/locate/config/screen.ini
    	plugins/locate/screen.py
    	plugins/settings/config/screen.ini
    	plugins/settings/screen.py
    	weather/config/screen.ini
    	weather/screen.py

--- gui_objects.py
+++ gui_objects.py
@@ -41,9 +41,6 @@ class text_label(pygame.sprite.Sprite):
         self.blit_text()
 
 
-# http://www.pygame.org/wiki/ShadowEffects?parent=CookBook
-<<<<<<< HEAD
-
 def DrawRoundRect(surface, color, rect, width, xr, yr):
     clip = surface.get_clip()
     # left and right
@@ -64,42 +61,10 @@ def DrawRoundRect(surface, color, rect, width, xr, yr):
     surface.set_clip(clip.clip(rect.left, rect.bottom - yr, xr, yr))
     pygame.draw.ellipse(surface, color, pygame.Rect(
         rect.left, rect.bottom - 2 * yr, 2 * xr, 2 * yr), width)
-=======
 
-def DrawRoundRect(surface, color, rect, width, xr, yr):
-
-    clip = surface.get_clip()
-    # left and right
-    surface.set_clip(clip.clip(rect.inflate(0, -yr * 2)))
-    pygame.draw.rect(surface, color, rect.inflate(1 - width, 0), width)
-
-    # top and bottom
-    surface.set_clip(clip.clip(rect.inflate(-xr * 2, 0)))
-    pygame.draw.rect(surface, color, rect.inflate(0, 1 - width), width)
-
-    # top left corner
-    surface.set_clip(clip.clip(rect.left, rect.top, xr, yr))
-    corner = pygame.Rect(rect.left, rect.top, 2 * xr, 2 * yr)
-    pygame.draw.ellipse(surface, color, corner, width)
-
-    # top right corner
-    surface.set_clip(clip.clip(rect.right - xr, rect.top, xr, yr))
-    pygame.draw.ellipse(surface, color, pygame.Rect(
-        rect.right - 2 * xr, rect.top, 2 * xr, 2 * yr), width)
-
-    # bottom left
-    surface.set_clip(clip.clip(rect.left, rect.bottom - yr, xr, yr))
-    pygame.draw.ellipse(surface, color, pygame.Rect(
-        rect.left, rect.bottom - 2 * yr, 2 * xr, 2 * yr), width)
-
->>>>>>> origin/master
-    # bottom right
     surface.set_clip(clip.clip(rect.right - xr, rect.bottom - yr, xr, yr))
     pygame.draw.ellipse(surface, color, pygame.Rect(
         rect.right - 2 * xr, rect.bottom - 2 * yr, 2 * xr, 2 * yr), width)
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/master
     surface.set_clip(clip)
     return clip
